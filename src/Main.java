b2c54c5517b4419b4513b14e5f4da5a393875cf1
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-04-05 11:38:34 -0300

    feat: Abstração

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ import models.Carro;
 public class Main {
     public static void main(String[] args) {
         Carro carro1 = new Carro();
+        carro1.Id = 777L;
         carro1.modelo = "Toyota Corolla";
         carro1.ano = 2022;
         carro1.cor = "Branco";
         carro1.exibirInfo();
+
+        System.out.println("Conceito de Abstração ------------------------");
+
+        carro1.moverVeiculo();
     }
 }
--- src/models/Carro.java
+++ src/models/Carro.java
@@ -1,11 +1,19 @@
 package models;
 
-public class Carro {
+public class Carro extends Veiculo {
+    public Long Id;
     public String modelo;
     public int ano;
     public String cor;
 
     public void exibirInfo() {
-        System.out.println("Modelo: " + modelo + ", Ano: " + ano + ", Cor: " + cor);
+        System.out.println("ID: " + Id + ", Modelo: " + modelo + ", Ano: " + ano + ", Cor: " + cor);
+    }
+
+
+    @Override
+    public void moverVeiculo() {
+        System.out.println("Carro em movimento");
+        System.out.println("Carro em alta velocidade");
     }
 }
